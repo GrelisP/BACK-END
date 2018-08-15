dfa5a0a4f2a71614c30021759f948eb17818793e
Povilas <40803841+GrelisP@users.noreply.github.com>
2018-08-15 22:35:26 +0300

    Update phpone.php

--- hm_W5_php_FEEDBACK/phpone.php
+++ hm_W5_php_FEEDBACK/phpone.php
@@ -7,41 +7,6 @@ $num2=isset($_POST['num2'])? $_POST['num2']:'';
 $sum;
 
 
-/*
-if ( strlen ( $name ) > 0 ){
-    $name=true;
-}else{
-    $name=false;
-}
-
-if ( strpos ( $email, '@' ) !==0 ){
-    $email=true;
-}
-else {
-    $email=false;
-}
-
-if ( strlen ( $feedback) > 0) {
-    $feedback=true;
-}else{
-    $feedback=false;
-}
-
-if ( $num1 + $num2)==0 {
-    $sum=true;
-}else{
-    $sum=false;
-}
-
-if ( $sum= $name= $email= $feedback= true){
-    print ("Thank you for giving a feedback");
-}else{
-    print ("There is an error");
-}
-
-*/
-//next try
-
 if ( strlen ( $name ) == 0 ){
     print (" There is an error");    
 }elseif (strpos ( $email, '@' ) == 0 ) {
